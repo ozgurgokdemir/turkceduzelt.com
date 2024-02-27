c152f65717112bfb04ad653e742642e0b91b5447
ufuk murat sınavu <u.murat0606@hotmail.com>
2024-02-27 14:07:48 +0300

    feat: add completions controller

---
+++ server/Controllers/CompletionsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using server.Services;
+
+namespace server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompletionsController : ControllerBase
+    {
+        private readonly ApiService _apiService;
+
+        public CompletionsController(ApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] string prompt)
+        {
+            if (string.IsNullOrEmpty(prompt))
+            {
+                return BadRequest("Prompt cannot be empty.");
+            }
+
+            string response = await _apiService.SendApiMessage(prompt);
+            return Ok(response);
+        }
+    }
+}
