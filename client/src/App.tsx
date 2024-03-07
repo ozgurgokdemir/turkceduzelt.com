267a1234a5c3c4187a5b33ef909f4f3a3b05687b
Özgür Gökdemir <ozgurgokd@gmail.com>
2024-03-07 22:29:36 +0300

    style: prettify codebase

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <h1 className="font-bold text-3xl underline">Hello world!</h1>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
