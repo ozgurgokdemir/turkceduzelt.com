2ec73ec59a05ecb24b8222d68cfe299ec63ef156
Özgür Gökdemir <ozgurgokd@gmail.com>
2024-03-12 01:36:22 +0300

    feat: add design system buttons

--- client/src/components/ui/Button/Button.tsx
+++ client/src/components/ui/Button/Button.tsx
@@ -1,54 +1,55 @@
 import * as React from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
+import { Typography } from '@/components/ui';
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center gap-3 whitespace-nowrap rounded-lg ring-offset-bg-surface transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-focus focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
-        destructive:
-          'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-        outline:
-          'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
-        secondary:
-          'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline',
+        filled: 'bg-fill text-primary hover:bg-fill-hover',
+        outline: 'border border-primary text-primary hover:bg-fill',
+        ghost: 'text-primary hover:bg-fill',
+        brand: 'bg-fill-brand text-brand-on-bg-fill hover:bg-fill-brand-hover',
+        success:
+          'bg-fill-success text-success-on-bg-fill hover:bg-fill-success-hover',
+        critical:
+          'bg-fill-critical text-critical-on-bg-fill hover:bg-fill-critical-hover',
       },
       size: {
-        default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
-        icon: 'h-10 w-10',
+        default: 'h-9 px-4',
+        icon: 'h-9 w-9',
       },
     },
     defaultVariants: {
-      variant: 'default',
+      variant: 'filled',
       size: 'default',
     },
   },
 );
 
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
-  asChild?: boolean;
-}
+type ButtonProps = React.ComponentPropsWithRef<'button'> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean;
+  };
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : 'button';
+  ({ asChild = false, variant, size, className, ...props }, ref) => {
+    const Component = asChild ? Slot : 'button';
+
     return (
-      <Comp
-        className={buttonVariants({ variant, size, className })}
-        ref={ref}
-        {...props}
-      />
+      <Typography variant="body-sm" asChild>
+        <Component
+          className={buttonVariants({ variant, size, className })}
+          ref={ref}
+          {...props}
+        />
+      </Typography>
     );
   },
 );
+
 Button.displayName = 'Button';
 
-export { Button, buttonVariants };
+export default Button;
