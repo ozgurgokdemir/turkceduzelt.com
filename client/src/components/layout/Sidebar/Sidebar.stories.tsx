6d4eab771ca5a3a5133ff69fd544b29adcd850c3
Özgür Gökdemir <ozgurgokd@gmail.com>
2024-03-13 08:10:42 +0300

    chore(storybook): add sidebar stories

---
+++ client/src/components/layout/Sidebar/Sidebar.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import SidebarComponent from './Sidebar';
+
+const meta = {
+  title: 'layout/Sidebar',
+  component: SidebarComponent,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof SidebarComponent>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Sidebar: Story = {
+  render: () => <SidebarComponent className="w-96" />,
+};
