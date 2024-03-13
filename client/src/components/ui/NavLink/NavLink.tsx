e98552d1fb26636c16fb9707952a7b37529d23c9
Özgür Gökdemir <ozgurgokd@gmail.com>
2024-03-13 05:56:10 +0300

    feat: add navlink component

---
+++ client/src/components/ui/NavLink/NavLink.tsx
@@ -0,0 +1,42 @@
+import type { LucideIcon } from 'lucide-react';
+import * as React from 'react';
+import { cx } from 'class-variance-authority';
+
+type NavLinkProps = React.ComponentPropsWithRef<'a'> & {
+  title: string;
+  icon: LucideIcon;
+  active?: boolean;
+  disabled?: boolean;
+};
+
+const NavLink = React.forwardRef<HTMLAnchorElement, NavLinkProps>(
+  (
+    {
+      title,
+      icon: Icon,
+      active = false,
+      disabled = false,
+      className,
+      ...props
+    },
+    ref,
+  ) => (
+    <a
+      className={cx(
+        'inline-flex h-12 items-center justify-center gap-4 whitespace-nowrap rounded-lg px-4 ring-offset-bg-surface transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-focus focus-visible:ring-offset-2',
+        active ? 'bg-surface-active' : 'bg-surface hover:bg-surface-hover',
+        disabled && 'pointer-events-none opacity-50',
+        className,
+      )}
+      ref={ref}
+      {...props}
+    >
+      <Icon size={20} strokeWidth={1.5} className="icon-primary" />
+      {title}
+    </a>
+  ),
+);
+
+NavLink.displayName = 'NavLink';
+
+export default NavLink;
