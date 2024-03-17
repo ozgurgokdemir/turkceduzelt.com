5917bd55c0b66acd14930e8b88af0efe8963a741
Özgür Gökdemir <ozgurgokd@gmail.com>
2024-03-17 07:47:12 +0300

    chore(suggestion): add sentence variant stories

--- client/src/features/suggestions/components/Suggestion/Suggestion.stories.tsx
+++ client/src/features/suggestions/components/Suggestion/Suggestion.stories.tsx
@@ -8,17 +8,35 @@ const meta = {
     layout: 'centered',
   },
   tags: ['autodocs'],
+  argTypes: {
+    variant: {
+      control: 'select',
+    },
+  },
 } satisfies Meta<typeof Suggestion>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  name: 'Suggestion',
+export const Word: Story = {
   args: {
+    variant: 'word',
     incorrect: 'incorrect',
     correct: 'correct',
+  },
+  render: (args) => (
+    <div className="w-96">
+      <Suggestion className="w-full" {...args} />
+    </div>
+  ),
+};
+
+export const Sentence: Story = {
+  args: {
+    variant: 'sentence',
+    incorrect: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    correct: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
   },
   render: (args) => (
     <div className="w-96">
