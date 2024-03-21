e1395ee2c81f7854727e7cf0d91cb6b623e6a02c
ufuk murat sınavu <u.murat0606@hotmail.com>
2024-03-21 15:12:36 +0300

    feat: add paraphraser controller

---
+++ server/Controllers/ParaphraserController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using server.Services;
+using static server.Controllers.CompletionsController;
+
+namespace server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParaphraserController : ControllerBase 
+    {
+        private readonly ApiService _apiService;
+
+        public ParaphraserController(ApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
+        readonly string paraphraserMessageFormal = @"sen türkçede uzmanlaşmış bir asistansın ve görevin sana verdiğimiz
+        bilgilere göre metinleri dönüştürmek. Türkçedeki tüm metinlerin resmi, nötr ve arkadaş canlısı olmak üzere üç seviyesi
+         vardır ve senin görevin sana verilen metinleri resmi olacak şekilde dönüştürmek.";
+        readonly string paraphraserMessageNotr = @"sen türkçede uzmanlaşmış bir asistansın ve görevin sana verdiğimiz
+        bilgilere göre metinleri dönüştürmek. Türkçedeki tüm metinlerin resmi, nötr ve arkadaş canlısı olmak üzere üç seviyesi
+         vardır ve senin görevin sana verilen metinleri notr olacak şekilde dönüştürmek.";
+        readonly string paraphraserMessageFriendly = @"sen türkçede uzmanlaşmış bir asistansın ve görevin sana verdiğimiz
+        bilgilere göre metinleri dönüştürmek. Türkçedeki tüm metinlerin resmi, nötr ve arkadaş canlısı olmak üzere üç seviyesi
+         vardır ve senin görevin sana verilen metinleri arkadaş canlısı olacak şekilde dönüştürmek.";
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] string prompt, [FromQuery] int value)
+        {
+            string systemMessage;
+            if (string.IsNullOrEmpty(prompt))
+            {
+                return BadRequest("Prompt cannot be empty.");
+            }
+
+            if(value == 1)
+            {
+                systemMessage = paraphraserMessageFormal;
+            }
+            else if(value == 2)
+            {
+                systemMessage = paraphraserMessageNotr;
+            }
+            else if (value == 3)
+            {
+                systemMessage = paraphraserMessageFriendly;
+            }
+            else
+            {
+                return BadRequest("false value");
+            }
+
+            string response =  await _apiService.SendParaphraserMessage(prompt, systemMessage); ;
+
+            return Ok(response);
+        }
+
+       
+    }
+}
