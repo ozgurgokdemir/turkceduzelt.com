e4cb1239480835e071523877209690832d42eb2e
ufuk murat sınavu <u.murat0606@hotmail.com>
2024-03-22 13:18:13 +0300

    feat: add Summarizer controller

---
+++ server/Controllers/SummarizerController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using server.Services;
+
+namespace server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SummarizerController : ControllerBase
+    {
+        private readonly ApiService _apiService;
+
+        public SummarizerController(ApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
+        readonly string systemMessage = @"sen türkçede uzmanlaşmış bir asistansın ve görevin sana verdiğimiz metinleri anlam
+          kaybına uğramayacak şekilde özetlemek.";
+
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] string prompt)
+        {
+          
+            if (string.IsNullOrEmpty(prompt))
+            {
+                return BadRequest("Prompt cannot be empty.");
+            }           
+
+            string response = await _apiService.SendSummarizerMessage(prompt, systemMessage); ;
+
+            return Ok(response);
+        }
+    }
+}
