86d63f55d86cbd902564e3000d26b209b0defb67
Özgür Gökdemir <ozgurgokd@gmail.com>
2024-03-23 03:39:17 +0300

    chore: add react query

--- client/src/providers/AppProvider.tsx
+++ client/src/providers/AppProvider.tsx
@@ -1,12 +1,22 @@
 import * as React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { EditorProvider } from '@/features/editor';
+import { SuggestionsProvider } from '@/features/suggestions';
+
+const queryClient = new QueryClient();
 
 type AppProviderProps = {
   children: React.ReactNode;
 };
 
 function AppProvider({ children }: AppProviderProps) {
-  return <EditorProvider>{children}</EditorProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <EditorProvider>
+        <SuggestionsProvider>{children}</SuggestionsProvider>
+      </EditorProvider>
+    </QueryClientProvider>
+  );
 }
 
 export default AppProvider;
