cb77b4a2734d74d004515971f1586845a676718a
Özgür Gökdemir <ozgurgokd@gmail.com>
2024-03-28 10:39:40 +0300

    chore(skeleton): add stories

---
+++ client/src/components/ui/Skeleton/Skeleton.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Skeleton from './Skeleton';
+
+const meta = {
+  title: 'ui/Skeleton',
+  component: Skeleton,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Skeleton>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  name: 'Skeleton',
+  render: () => (
+    <div className="flex items-center space-x-4">
+      <Skeleton className="h-12 w-12 rounded-full" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px]" />
+        <Skeleton className="h-4 w-[200px]" />
+      </div>
+    </div>
+  ),
+};
