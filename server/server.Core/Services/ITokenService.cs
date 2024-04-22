9a32674a902816e7142bbb07a37f5055bc91f854
ufuk murat sınavu <u.murat0606@hotmail.com>
2024-04-22 22:22:10 +0300

    feat: add ITokenService

---
+++ server/server.Core/Services/ITokenService.cs
@@ -0,0 +1,15 @@
+﻿using server.Core.DTOs;
+using server.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server.Core.Services
+{
+    public interface ITokenService
+    {
+        TokenDto CreateToken(UserApp userApp);
+    }
+}
