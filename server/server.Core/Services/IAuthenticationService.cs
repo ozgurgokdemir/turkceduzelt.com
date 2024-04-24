315474e58641a410e82789273d6ec55d2d7b9b12
ufuk murat sınavu <u.murat0606@hotmail.com>
2024-04-24 13:09:04 +0300

    feat: add IAuthenticationService

---
+++ server/server.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,19 @@
+﻿using server.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server.Core.Services
+{
+    public interface IAuthenticationService
+    {
+        Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto);
+
+        Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
+
+        Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken);
+
+    }
+}
