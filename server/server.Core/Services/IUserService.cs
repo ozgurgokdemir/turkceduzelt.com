4ff8bd89b966a38a1d2570446f007358c93d96e9
ufuk murat sınavu <u.murat0606@hotmail.com>
2024-04-24 13:20:40 +0300

    feat: add IUserService

---
+++ server/server.Core/Services/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using server.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server.Core.Services
+{
+    public interface IUserService
+    {
+        Task<Response<UserAppDto>> CreateUserAsync(CreateUserDto createUserDto);
+
+        Task<Response<UserAppDto>> GetUserByNameAsync(string userName);
+    }
+}
