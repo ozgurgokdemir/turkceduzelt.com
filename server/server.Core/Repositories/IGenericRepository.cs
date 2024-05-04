f5d3ac29a34b9a1fafe0a199a757dec1721cd896
ufuk murat sınavu <u.murat0606@hotmail.com>
2024-05-04 15:08:19 +0300

    feat: add generic service class

--- server/server.Core/Repositories/IGenericRepository.cs
+++ server/server.Core/Repositories/IGenericRepository.cs
@@ -14,5 +14,6 @@ namespace server.Core.Repositories
         IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
         Task AddAsync(TEntity entity);
         void Remove(TEntity entity);
+        TEntity Update(TEntity entity);
     }
 }
