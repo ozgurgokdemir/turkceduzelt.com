777132523ee9b7618f3bf668a52dc253642fba91
ufuk murat sınavu <u.murat0606@hotmail.com>
2024-05-06 00:14:26 +0300

    refactor: move layers

---
+++ server/server/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using server.Core.DTOs;
+using server.Core.Services;
+
+namespace server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(CreateUserDto createUserDto)
+        {
+           var result = await _userService.CreateUserAsync(createUserDto);
+            if(result.IsSuccessful)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> GetUser()
+        {
+            var result = await _userService.GetUserByNameAsync(HttpContext.User.Identity.Name);
+            if (result.IsSuccessful)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ server/server/Services/IApiService.cs
@@ -0,0 +1,10 @@
+﻿namespace server.Services
+{
+    public interface IApiService
+    {
+        public Task<string> SendApiMessage(string prompt);
+        public Task<string> SendParaphraserMessage(string prompt, string systemMessage);
+        public Task<string> SendSummarizerMessage(string prompt, string systemMessage);
+
+    }
+}
