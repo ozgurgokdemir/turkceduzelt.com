31fc006a24076e0eec7f583de43e83f0a7bc5fb1
Özgür Gökdemir <ozgurgokd@gmail.com>
2024-05-14 16:34:16 +0300

    chore(storybook): add spinner

---
+++ client/src/components/ui/Spinner/Spinner.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Spinner from './Spinner';
+
+const meta = {
+  title: 'ui/Spinner',
+  component: Spinner,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    size: {
+      control: 'select',
+    },
+  },
+} satisfies Meta<typeof Spinner>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Small: Story = {
+  args: {
+    size: 'sm',
+  },
+};
+
+export const Medium: Story = {
+  args: {
+    size: 'md',
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: 'lg',
+  },
+};
