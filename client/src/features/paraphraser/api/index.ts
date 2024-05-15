51c89f155d7449bde3cf14c19635de0ce9d67d83
Özgür Gökdemir <ozgurgokd@gmail.com>
2024-05-15 04:22:11 +0300

    feat(paraphraser): add post api

---
+++ client/src/features/paraphraser/api/index.ts
@@ -0,0 +1,1 @@
+export { default as postParaphraser } from './post-paraphraser';
---
+++ client/src/features/paraphraser/api/post-paraphraser.ts
@@ -0,0 +1,20 @@
+async function postParaphraser(text: string) {
+  const response = await fetch(
+    'https://localhost:7256/api/paraphraser?value=2',
+    {
+      method: 'POST',
+      body: JSON.stringify(text),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+  );
+
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+
+  return response.text();
+}
+
+export default postParaphraser;
